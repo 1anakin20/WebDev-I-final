421fa829a2a8fd50fa532e5004879aadc38cce4c
DuReaLDuCK <72211275+DuReaLDuCK@users.noreply.github.com>
2020-12-16 01:39:53 -0500

    Siderbar height fixed
    Added a functoin to change height of size bar when the window is resized
    Added the same lines to the set up function

--- site/js/navigation.js
+++ site/js/navigation.js
@@ -3,8 +3,18 @@
  */
 function clock() {
     let date = new Date();
-    let time = date.getHours() + ":";
+    let hours = date.getHours();
     let minutes = date.getMinutes();
+    let time = "";
+
+    if (hours < 10) {
+        time += "0" + hours;
+    } else {
+        time += hours;
+    }
+
+    time += ":";
+
     if (minutes < 10) {
         time += "0" + minutes;
     } else {
@@ -19,7 +29,7 @@ function clock() {
 /**
  * Sets the year in the menu bar
  * @param {string} year - Year to display, leave the string empty for current year
- */
+ **/
 function setYear(year = "") {
     let yearElement = document.querySelector("#year");
 
@@ -34,11 +44,23 @@ function setYear(year = "") {
 function setup() {
     let about = document.querySelector("#about");
     about.addEventListener("click", function () {
-        window.alert("Authors: Joseph Dubrovsky, Marian Hristov, Tomas Daniel Nieto.\n2020")
+        window.alert("Authors:\nJoseph Dubrovsky, Marian Hristov, Tomas Daniel Nieto.\n2020");
     });
+
+    // * Changing the height of the sidebar based on the height of the window
+    $("main").css("height", $(window).height() - $("nav").height() + "px");
+    $("aside").css("height", "100%");
+    $("#sidebar").css("height", "100%");
 
     clock();
     setYear();
 }
+
+// * Changing the height of the sidebar based on the height of the window after resize
+$(window).resize(function () {
+    $("main").css("height", $(window).height() - $("nav").height() + "px");
+    $("aside").css("height", "100%");
+    $("#sidebar").css("height", "100%");
+});
 
 window.addEventListener("load", setup);
