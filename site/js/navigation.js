66546155d07be58341bff7f67f439674f4c32554
1anakin20 <33395451+1anakin20@users.noreply.github.com>
2020-12-20 20:18:48 -0500

    Nav bar (#3)
    * Top navigation bar
    
    * updated gitignore for vscode
    
    * Made the sidebar
    
    * Added texture to the navigation bars
    
    * Animated hover on the sidebar links
    
    * Simplified expression
    
    * Changed opaque to its own class; navbar hover animated
    
    * Removed padding on navbar
    
    * Added border on sidebar
    
    * Renamed references of nav-bar to navigation
    
    * JS for navigation: update the time; set the year
    
    * Changed type of the year parameter
    
    * Changed from innerHTML to textContent to set the year
    
    * Changed to adding an event listener
    
    * Dropdown menu on logo icon
    
    * Changed keyframes to top
    
    * About box alert
    
    * Corrected a name
    
    * Clock, Sidebar Headings, Details
    
    js: changed the clock text format
    css: removed the styling of headings in the side bar. changed the order of some elements for a neater look
    html: added main container and footer, also added icons and list items for pages of navbar
    
    * Navbar changes
    
    Completly changed the styling of the navbar, looks bigger and more spaced
    
    * Addition of jquery
    
    added jquery in head of html
    
    * Siderbar height fixed
    
    Added a functoin to change height of size bar when the window is resized
    Added the same lines to the set up function
    
    * Fixed recursion when updating the clock
    
    * Specified the use of the window class
    
    * Positionning of major elements
    
    Added footer
    Added random filler content
    Changed the siderbar and the main content so they're side to side
    
    * Sidebar and navbar following frame
    
    Made the sidebar and navbar sticky so they always stop on screen
    
    * Placeholder content
    
    took off some placeholder content
    
    * Footer styling
    
    changed footer styling added some spacing and a line between head and body of table
    
    * Added event sound when a link is clicked
    
    * Added comment
    
    * Added toggle to mute the sounds effects
    
    * Fixed sidebar being over the menubar
    
    * Added favicon
    
    * Removed Z-index
    
    * Sidebar bug fix
    
    Fixed the bug of the sidebar going over the navbar when the height of the window is too small
    Removed the last separator of the sidebar
    
    * Student numbers
    
    Added student numbers and sonic pages
    Added Copyright logo in the footer
    
    * Removed sounds
    
    * Added meta tags
    
    * Rename navigation.css menu.css
    
    * Set first item to home
    
    * Renamed navigation to template
    
    Co-authored-by: DuReaLDuCK <72211275+DuReaLDuCK@users.noreply.github.com>

---
+++ site/js/navigation.js
@@ -0,0 +1,73 @@
+/**
+ *  Updates the time each minute 
+ */
+function clock() {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let time = "";
+
+    if (hours < 10) {
+        time += "0" + hours;
+    } else {
+        time += hours;
+    }
+
+    time += ":";
+
+    if (minutes < 10) {
+        time += "0" + minutes;
+    } else {
+        time += minutes;
+    }
+
+    document.querySelector("#clock").innerHTML = time;
+}
+
+/**
+ * Sets the year in the menu bar
+ * @param {string} year - Year to display, leave the string empty for current year
+ **/
+function setYear(year = "") {
+    let yearElement = document.querySelector("#year");
+
+    if (year == "") {
+        let currentYear = new Date().getFullYear().toString();
+        yearElement.textContent = currentYear;
+    } else {
+        yearElement.textContent = year;
+    }
+}
+
+/**
+ * Plays sounds when items are clicked in the website
+ * @param {*} event 
+ */
+function playSoundNavigation(event) {
+    if (!muted && event.target.tagName === 'A') {
+        let audio = new Audio("/site/sound/popp.mp3")
+        audio.play();
+    }
+}
+
+function setup() {
+    let about = document.querySelector("#about");
+    about.addEventListener("click", function () {
+        window.alert("Authors:\nJoseph Dubrovsky, Marian Hristov, Tomas Daniel Nieto.\n2020");
+    });
+
+    // * Changing the height of the sidebar based on the height of the window
+    $("#sidebar>ul").css("top", $("header").height() + "px");
+
+    clock();
+    // Update each second
+    window.setInterval(clock, 1000);
+    setYear();
+}
+
+// * Changing the height of the sidebar based on the height of the window after resize
+$(window).resize(function () {
+    $("#sidebar>ul").css("top", $("header").height() + "px");
+});
+
+window.addEventListener("load", setup);
