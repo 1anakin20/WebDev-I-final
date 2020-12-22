51ee41667e292e09853dad9bd37fbfe4ef134fe9
1anakin20 <33395451+1anakin20@users.noreply.github.com>
2020-12-22 17:51:12 +0000

    Updated navigation

--- site/js/navigation.js
+++ site/js/navigation.js
@@ -58,7 +58,7 @@ function setup() {
 
     // * Changing the height of the sidebar based on the height of the window
     $("#sidebar>ul").css("top", $("header").height() + "px");
-
+    $("*, html").css("scroll-margin-top", $("header").height() + parseInt($("main>.window>.window-body>article>h2").css("margin-top")) + "px");
     clock();
     // Update each second
     window.setInterval(clock, 1000);
