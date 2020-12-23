271cddb6bb011c08d1f78b0a01da9db5b83f1d07
1anakin20 <33395451+1anakin20@users.noreply.github.com>
2020-12-23 13:18:28 -0500

    Home (#5)
    * Added popup covid message
    
    * Added background for popup
    
    Added a dark semi transparent background for the covid popup
    
    * Added template.html
    
    * Updated nagivation.js
    
    * Covid prompt
    
    Changed styling and content of covid prompt
    
    * Fixed pop occupying the full height
    
    * Content
    
    Added contents of home page
    Added typewriter effect for welcome message
    
    Co-authored-by: DuReaLDuCK <72211275+DuReaLDuCK@users.noreply.github.com>

---
+++ site/js/home.js
@@ -0,0 +1,50 @@
+function closePopup() {
+    let popup = document.querySelector("#wrapper-popup");
+    popup.style.display = "none";
+
+    var h2 = {
+        text: "Welcome!",
+        obj: document.querySelector("h2.typewriter")
+    };
+    var text1 = {
+        text: "Welcome to our website!",
+        obj: document.querySelector("#text1")
+    };
+    var text2 = {
+        text: "It contains many facts about computer history.",
+        obj: document.querySelector("#text2")
+    };
+    var text3 = {
+        text: "Please enjoy your stay and have fun reading!",
+        obj: document.querySelector("#text3")
+    };
+
+
+    setTimeout(500);
+    typewriter(h2.text, h2.obj, 0, 100);
+    typewriter(text1.text, text1.obj, 0, 80);
+    typewriter(text2.text, text2.obj, 0, 80);
+    typewriter(text3.text, text3.obj, 0, 80);
+}
+
+function setup() {
+    let close = document.querySelectorAll(".close-popup");
+    console.log(close);
+    for (const e of close) {
+        e.addEventListener("click", closePopup);
+    }
+}
+
+function typewriter(text, obj, i, speed) {
+    if (i < text.length) {
+        obj.textContent += text.charAt(i);
+        i++
+        setTimeout(function () {
+            typewriter(text, obj, i)
+        }, speed);
+    }
+
+}
+
+
+window.addEventListener("load", setup);
